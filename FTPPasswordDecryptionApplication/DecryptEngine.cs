3d35c15aef7be8ec3c3ad63c0ab3ec7439ceaf71
maciej.wolejszo <maciej.wolejszo@gmail.com>
2019-03-22 01:51:47 +0100

    FTP Password Generator

---
+++ FTPPasswordDecryptionApplication/DecryptEngine.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace FTPPasswordDecryptionApplication
+{
+    public class DecryptEngine
+    {
+        private static readonly byte[] CryptoKey = Encoding.ASCII.GetBytes("FtPa$$w0rD");
+        private static readonly byte[] CryptoStandardIv = Encoding.ASCII.GetBytes("C0nn3ci0n");
+        private static readonly byte[] MobileCryptoStandardIv = Encoding.ASCII.GetBytes("C0nn3ci0n");
+        private static readonly byte[] CryptoPasswordIv = Encoding.ASCII.GetBytes("Pa55w0rD");
+        public const string SecureConnectionProviderName = "SecureConnection";
+
+        static private string EncryptString(string value, byte[] encryotionIv)
+        {
+            byte[] inBytes = Encoding.Unicode.GetBytes(value);
+            RC2CryptoServiceProvider csp = new RC2CryptoServiceProvider();
+            ICryptoTransform encryptor = csp.CreateEncryptor(CryptoKey, encryotionIv);
+
+            byte[] outByte = encryptor.TransformFinalBlock(inBytes, 0, inBytes.Length);
+            return Convert.ToBase64String(outByte);
+        }
+
+        static private string DecryptString(string value, byte[] encryotionIv)
+        {
+            byte[] inBytes = Convert.FromBase64String(value);
+            RC2CryptoServiceProvider csp = new RC2CryptoServiceProvider();
+            ICryptoTransform decryptor = csp.CreateDecryptor(CryptoKey, encryotionIv);
+
+            byte[] outByte = decryptor.TransformFinalBlock(inBytes, 0, inBytes.Length);
+            return Encoding.Unicode.GetString(outByte);
+        }
+        public static string EncryptFtpPassword(string value)
+        {
+            //æ - Alt+145
+            //(UtcDate | Password | UtcTime)
+            return EncryptString(string.Format("{0}æ{1}æ{2}æ{3}", DateTime.UtcNow.ToString("yyyy-MM-dd"),
+                value, DateTime.UtcNow.ToString("HH:mm:ss"), "FTP"), CryptoPasswordIv);
+        }
+        public static string DecryptFtpPassword(string value)
+        {
+            //æ - Alt+145
+            string[] val = DecryptString(value, CryptoPasswordIv).Split('æ');
+            DateTime returnDate;
+            if (val.Length == 4) //must have 3 sets (UtcDate | Password | UtcTime | FTP)
+                if (DateTime.TryParse(val[0] + ' ' + val[2], out returnDate)) //Should be a valid date
+                    if (val[3] == "FTP")
+                        return val[1];
+
+            return null;
+        }
+    }
+}
---
+++ FTPPasswordDecryptionApplication/FTPPAsswordDEcyptor.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FTPPasswordDecryptionApplication
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class FTPPasswordDecyptor : Window
+    {
+        public FTPPasswordDecyptor()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+        }
+
+        private void TextBox2_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+        }
+
+        private void TextBox3_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+        }
+
+        private void Decrypt_Click(object sender, RoutedEventArgs e)
+        {
+            if (TextBox2.SelectedText != null)
+            {
+                TextBox3.Text = DecryptEngine.DecryptFtpPassword(TextBox2.Text);
+            }
+            else
+                MessageBox.Show("This field cannot be empty!");
+        }
+    
+
+        private void Encrypt_Click(object sender, RoutedEventArgs e)
+        {
+            if ()
+            {
+                TextBox2.Text = DecryptEngine.EncryptFtpPassword(TextBox1.Text);
+            }
+            else
+                MessageBox.Show("This field cannot be empty!");
+        }
+    }
+}
